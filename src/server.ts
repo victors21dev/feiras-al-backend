5d25b7fd12be95eef6a2c9b1abe5047bf8f41426
Victor Santos <santos.victors2000@gmail.com>
2025-09-25 01:33:30 -0300

    fix: correction cors to add form mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,29 @@
 import express from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 import eventRoutes from "./routes/eventRoutes";
 import userRoutes from "./routes/userRoutes";
 import { clerkMiddleware } from '@clerk/express';
+
+const allowedOrigins: string[] = [
+  `${process.env.URL_SERVER_FRONT}`,
+];
+
+const corsOptions: CorsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    // A condição !origin permite requisições sem origem (como Postman/Insomnia)
+    // O '!' em origin! diz ao TypeScript que, neste ponto, origin não será undefined
+    if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Acesso não permitido por CORS'));
+    }
+  }
+};
+
+const app2 = express();
+app2.use(cors(corsOptions));
 
 dotenv.config();
 
@@ -19,6 +38,8 @@ app.use(clerkMiddleware());
 connectDB();
 app.use("/api/events", eventRoutes);
 app.use('/api/users', userRoutes);
+
+
 
 app.listen(port, () => {
   console.log(`🚀 Servidor rodando na porta ${port}`);
