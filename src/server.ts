ba6aff3bab18ef14a8ad1509972fb270aaf2ecaf
Victor Santos <santos.victors2000@gmail.com>
2025-09-25 01:44:56 -0300

    fix: erro cors to server

--- src/server.ts
+++ src/server.ts
@@ -7,30 +7,15 @@ import userRoutes from "./routes/userRoutes";
 import { clerkMiddleware } from '@clerk/express';
 
 const allowedOrigins: string[] = [
-  `${process.env.URL_SERVER_FRONT}`,
+  'https://feiras-al.vercel.app',
+  'http://localhost:5173'
 ];
-
-const corsOptions: CorsOptions = {
-  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    // A condição !origin permite requisições sem origem (como Postman/Insomnia)
-    // O '!' em origin! diz ao TypeScript que, neste ponto, origin não será undefined
-    if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Acesso não permitido por CORS'));
-    }
-  }
-};
-
-const app2 = express();
-app2.use(cors(corsOptions));
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: `${process.env.URL_SERVER_FRONT}` }));
 app.use(cors())
 app.use(express.json());
 app.use(clerkMiddleware());
