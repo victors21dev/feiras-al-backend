42df4704ec7463775002285025ff8a7c8af9191e
Victor Santos <santos.victors2000@gmail.com>
2025-09-25 01:10:57 -0300

    fix: update to server to err cors

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({ origin: `${process.env.URL_SERVER_FRONT}` }));
+app.use(cors())
 app.use(express.json());
 app.use(clerkMiddleware());
 
